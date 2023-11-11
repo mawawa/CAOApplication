4c15f71d6a270663c98bc0369e0774bf6f5803da
coffee <nanashongwe@gmail.com>
2023-11-11 03:37:44 +0200

    off to Ai

--- CAO/src/main/java/com/nano/CAO/computer/io/InputOutputController.java
+++ CAO/src/main/java/com/nano/CAO/computer/io/InputOutputController.java
@@ -1,14 +1,23 @@
 package com.nano.CAO.computer.io;
 
+import com.nano.CAO.QandA.QandAService;
+import com.nano.CAO.QandA.QuestionAnswer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class InputOutputController {
-
+    @Autowired
+    QandAService qandAService;
     @RequestMapping("io")
-    public ModelAndView io(){
-        return new ModelAndView("inputOutput/IO.html");
+    public ModelAndView io() {
+        ModelAndView modelAndView = new ModelAndView("inputOutput/IO.html");
+        QuestionAnswer newQuestionAnswer = new QuestionAnswer();
+        newQuestionAnswer.setTutorial(3);
+        modelAndView.addObject("newQandA", newQuestionAnswer);
+        modelAndView.addObject("qAndAs", qandAService.getQuestions(3));
+        return modelAndView;
     }
 }
